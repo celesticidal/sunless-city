3f1666c446dce533fde1d4c0bf40108fbb521543
celesticidal <celesticidal@proton.me>
2025-12-15 21:30:49 -0700

    added main.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://sunless.city"
+  site: "https://sunless.city",
+  integrations: [preact()]
 });
